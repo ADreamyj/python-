fb48673307c238fe676d7fa18a2ebed3870c9cc0
ADreamyj <861396671@qq.com>
2020-01-21 17:14:18 +0800

    python学习笔记

---
+++ python学习笔记/01python.py
@@ -0,0 +1,13 @@
+# print(100 + 200 +300)
+# print("hello world!")
+# print("100 + 200 =",100 + 200)
+# name = input()
+# name
+
+# name = input()
+# print("hello,",name)
+
+# name = input("please enter your name:")
+# print("hello,",name)
+
+print(1024 * 768)
