302222a3b3a88e0a8dae36649b3b290492998008
ADreamyj <861396671@qq.com>
2020-01-22 20:12:25 +0800

    02 - 1数据类型和变量.py

---
+++ python学习笔记/02 - 1数据类型和变量.py
@@ -0,0 +1,64 @@
+# a = 100
+# if a >= 0:
+#     print(a)
+# else:
+#    print(-a)
+
+# print("I\'am learning\nPython.")
+
+# print("\\\t\\")
+
+# print('''line1
+# ...line2
+# ...line3''')
+
+# ...为提示符
+
+# True
+# False
+# 3 > 2
+# 3 > 5
+
+
+'''
+age = 18
+if age >= 18:
+    print('adult')
+else:
+    print('teenager')
+'''
+
+'''
+x = 10
+x = x + 2;
+print(x)
+'''
+
+'''
+a = 'ABC'
+b = a;
+a = 'XYZ'
+print(a,b)
+'''
+
+n = 123
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+
+print(n)
+print(f)
+print(s1)
+print(s2)
+print(s3)
+print(s4)
+
+
+
+
+
+
+
